33f0e354084fad1ba2aacb726d6dee090d50b24b
bukomp <edvard@shalaev.com>
2019-08-26 00:26:08 +0300

    osa 4 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 
+
 blogRouter.get('', (request, response) => {
   Blog
     .find({})
@@ -9,15 +10,44 @@ blogRouter.get('', (request, response) => {
     })
 })
 
-blogRouter.post('', (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post('', (req, res, next) => {
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  if (typeof req.body.likes !== 'number') req.body.likes = 0
+  if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('url')) {
+    res.status(400).send({error: 'url or title is missing'})
+  } else {
+
+    const blog = new Blog(req.body)
+
+    blog
+      .save()
+      .then(result => {
+        res.status(201).json(result)
+      })
+      .catch(e => {
+        next(e)
+      })
+  }
 })
 
+blogRouter.delete('/:id', async (req, res) => {
 
-module.exports = blogRouter
+  const remove = await Blog.findByIdAndRemove(req.params.id)
+
+  if(remove === null) res.status(404).json({error:"no data found"})
+
+  res.status(204).send()
+})
+
+blogRouter.put('/:id', async (req, res) => {
+
+  if (typeof req.body.likes !== 'number') req.body.likes = 0
+
+  const update = await Blog.findByIdAndUpdate(req.params.id, req.body.likes)
+
+  if (update === null) res.status(404).json({error: "no data found"})
+
+  res.status(200).json(update)
+})
+
+module.exports = blogRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,23 +1,90 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog');
 
-test('blogs are returned as json', async () => {
-  const res = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  expect(res.body.length).toBe(2)
+describe('blog tests', () => {
 
+  test('blogs are returned as json', async () => {
+    const res = await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are actual amount of blogs', async () => {
+
+    const blogs = await Blog.find({})
+
+    const response = await api.get('/api/blogs')
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.length).toBe(blogs.length)
+  })
+
+  test('id in json', async () => {
+    const res = await api.get('/api/blogs')
+    res.body.forEach(r => {
+      console.log("+")
+      expect(r.id).toBeDefined()
+    })
+  })
+
+  test('posting blogs', async () => {
+    const data = {
+      title: "wowee",
+      author: "Marx",
+      url: "http://lol.com",
+      likes: 0
+    }
+
+    const blog = await Blog.find({})
+
+    await api.post('/api/blogs').send(data)
+
+    const res = await Blog.find({})
+
+    expect(res.length).toBe(blog.length+1)
+  })
+
+  test('likes = something or 0', async () => {
+    const data = {
+      title: "wowee",
+      author: "Marx",
+      url: "http://lol.com",
+      likes: ""
+    }
+
+    const res = await api.post('/api/blogs').send(data)
+    console.log(res.body.likes)
+    expect(res.body.likes).toBe(0)
+  })
+
+  test('Bad Reqest', async () => {
+    const data1 = {
+      author: "Marx",
+      url: "http://lol.com",
+      likes: 0
+    }
+
+    await api.post('/api/blogs')
+      .send(data1)
+      .expect(400)
+
+    const data2 = {
+      title: "wowee",
+      author: "Marx",
+      likes: 0
+    }
+
+    await api.post('/api/blogs')
+      .send(data2)
+      .expect(400)
+  })
+
+
+  afterAll(() => {
+    mongoose.connection.close()
+  })
 })
 
-test('there are four notes', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body.length).toBe(4)
-})
-
-afterAll(() => {
-  mongoose.connection.close()
-})
