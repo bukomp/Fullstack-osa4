3a3ae847fa27d37e34489fd55697428966cfbf89
bukomp <edvard@shalaev.com>
2019-08-25 13:37:01 +0300

    osa 4 4.8

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,12 +5,11 @@ const app = require('../app')
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-  await api.get('/api/blogs')
+  const res = await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-    .then(r =>{
-      expect(r.body.length).toBe(2)
-    })
+  expect(res.body.length).toBe(2)
+
 })
 
 test('there are four notes', async () => {
