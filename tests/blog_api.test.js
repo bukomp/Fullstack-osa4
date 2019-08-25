63d078e19a621c81c0639b4cc034775b4b871196
bukomp <edvard@shalaev.com>
2019-08-25 13:34:23 +0300

    osa 4 4.8

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,39 +5,12 @@ const app = require('../app')
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
+  await api.get('/api/blogs')
     .expect(200)
-    .expect([
-      {
-        "title": "wowwee",
-        "author": "String",
-        "url": "String",
-        "likes": 0,
-        "id": "5d5522e4606adb3ccc02bd7e"
-      },
-      {
-        "title": "wowwee",
-        "author": "String",
-        "url": "String",
-        "likes": 0,
-        "id": "5d591eda774d0c2a70556d46"
-      },
-      {
-        "title": "wowwee",
-        "author": "String",
-        "url": "String",
-        "likes": 0,
-        "id": "5d5922078a5a4e239438bd63"
-      },
-      {
-        "title": "wowwee",
-        "author": "String",
-        "url": "String",
-        "likes": 0,
-        "id": "5d59225a024eb224689a6893"
-      }
-    ])
+    .expect('Content-Type', /application\/json/)
+    .then(r =>{
+      expect(r.body.length).toBe(2)
+    })
 })
 
 test('there are four notes', async () => {
