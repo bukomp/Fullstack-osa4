cddf6d3af259a9d46930ab8e3ae47d5ea2074b52
bukomp <edvard@shalaev.com>
2019-09-24 13:55:42 +0300

    osa 4 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,15 +4,6 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 
 
-const getTokenFrom = req => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
-
 blogRouter.get('/', (request, response) => {
   Blog
     .find({})
@@ -24,50 +15,66 @@ blogRouter.get('/', (request, response) => {
 
 blogRouter.post('/', async (req, res, next) => {
   const body = req.body;
-  const token = getTokenFrom(req)
+  const token = req.token
 
+  console.log(req.body);
+  try {
 
-
-    console.log(req.body);
-    try {
-
-      const decodedToken = jwt.verify(token, process.env.SECRET)
-      if (!token || !decodedToken.id) {
-        return res.status(401).json({ error: 'token missing or invalid' })
-      }
-
-      const user = await User.findById(decodedToken.id)
-
-      if (typeof req.body.likes !== 'number') req.body.likes = 0
-
-      if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('url')) {
-        res.status(400).send({error: 'url or title is missing'})
-      }
-
-      const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-      })
-
-      const savedBlog = await blog.save()
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
-      res.status(201).json(savedBlog)
-
-    } catch (e) {
-      next(e)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return res.status(401).json({ error: 'token missing or invalid' })
     }
+
+    const user = await User.findById(decodedToken.id)
+
+    if (typeof req.body.likes !== 'number') req.body.likes = 0
+
+    if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('url')) {
+      res.status(400).send({error: 'url or title is missing'})
+    }
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    res.status(201).json(savedBlog)
+
+  } catch (e) {
+    next(e)
+  }
 })
 
-blogRouter.delete('/:id', async (req, res) => {
+blogRouter.delete('/:id', async (req, res, next) => {
+  try {
 
-  const remove = await Blog.findByIdAndRemove(req.params.id)
+    const decodedToken = jwt.verify(req.token, process.env.SECRET)
 
-  if(remove === null) res.status(404).json({error:"no data found"})
-  else res.status(204).send()
+    if (!req.token || !decodedToken.id) {
+      return res.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(req.params.id)
+
+    if (blog === null) return res.status(404).json({error: "no data found"})
+
+    if( blog !== null && user._id.toString() === blog.user.toString()) {
+      console.log("yee");
+      await blog.remove()
+    }
+
+    res.status(204).send()
+
+  } catch (e) {
+    next(e)
+  }
 })
 
 blogRouter.put('/:id', async (req, res) => {
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,14 @@
+
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -6,31 +17,36 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+const errorHandler = (e, req, res, next) => {
+  console.error(e.message)
 
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id'
+  if (e.name === 'CastError' && e.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id'
     })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message
+  } else if (e.name === 'ValidationError') {
+    return res.status(400).json({ error: e.message
     })
-  } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({
+  } else if (e.name === 'JsonWebTokenError') {
+    return res.status(401).json({
       error: 'invalid token'
     })
   }
 
-  next(error)
+  next(e)
 }
 
+const tokenExtractor = (req, res, next) => {
+  req.token = getTokenFrom(req)
+  next()
+}
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
